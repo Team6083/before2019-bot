c956b5fce56ecc34e74495b7c52ae6dc50f46831
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-12-12 19:42:34 +0800

    remove unused import

--- src/org/usfirst/frc/team6083/robot/auto/modes/Scale.java
+++ src/org/usfirst/frc/team6083/robot/auto/modes/Scale.java
@@ -2,8 +2,6 @@ package org.usfirst.frc.team6083.robot.auto.modes;
 
 import org.usfirst.frc.team6083.robot.auto.AutoEngine;
 
-import edu.wpi.first.wpilibj.Timer;
-
 public class Scale extends AutoEngine {
 
 	public static void loop() {
